195cc33731e577ae66b35db64a799d6313b39df7
tsxlts <tsxlts>
2025-04-18 15:53:38 +0800

    2.0.6.2 C5自动发送报价

---
+++ SteamAuthenticator/Model/C5/QuerySellerOrdersResponse.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Steam_Authenticator.Model.C5
+{
+    public class QuerySellerOrdersResponse
+    {
+        public int total { get; set; }
+        public int pages { get; set; }
+        public int page { get; set; }
+        public int limit { get; set; }
+        public List<C5Order> list { get; set; } = new List<C5Order>();
+    }
+
+    public class C5Order
+    {
+
+        public string orderId { get; set; }
+
+        public string productId { get; set; }
+
+        public string appId { get; set; }
+
+        public string itemId { get; set; }
+
+        public string name { get; set; }
+
+        public string marketHashName { get; set; }
+
+        public string imageUrl { get; set; }
+
+        public decimal price { get; set; }
+
+        public string status { get; set; }
+
+        public string statusName { get; set; }
+    }
+}
